2069b5c2812a97a120bebc6dfaf1cc454c3c3d17
Joseph M. Temesi <joemunyendo@gmail.com>
2023-09-13 11:29:57 +0300

    Added set attribute method to second player

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ function diceImage () {
     var randomNumber1 = randomNumbers[0];
     var randomNumber2 = randomNumbers[1];
     var image1 = document.querySelector(".img1");
+    var image2 = document.querySelector(".img2");
+    
     
     if (randomNumber1 == 1 ) {
          var image1 = image1.setAttribute("src", "Assets/images/dice1.png")
@@ -25,5 +27,20 @@ function diceImage () {
     } else {
          var image1 = image1.setAttribute("src", "Assets/images/dice6.png");
     }
-    
+
+    if (randomNumber2 == 1 ) {
+         var image2 = image2.setAttribute("src", "Assets/images/dice1.png")
+    } else if (randomNumber2 == 2) {
+         var image2 = image2.setAttribute("src", "Assets/images/dice2.png");
+    } else if (randomNumber2 == 3) {;
+         var image2 = image2.setAttribute("src", "Assets/images/dice3.png");         
+    } else if (randomNumber2 == 4) {
+         var image2 = image2.setAttribute("src", "Assets/images/dice4.png");
+    } else if (randomNumber2 == 5) {
+         var image2 = image2.setAttribute("src", "Assets/images/dice5.png");
+    } else {
+         var image2 = image2.setAttribute("src", "Assets/images/dice6.png");
+    }  
+
+
 }
