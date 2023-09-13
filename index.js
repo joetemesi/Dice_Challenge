04ee74c768502b1c7e0b625d62b61de9c6d78d31
Joseph M. Temesi <joemunyendo@gmail.com>
2023-09-13 11:20:25 +0300

    Added function that changes first player image attributes

--- index.js
+++ index.js
@@ -2,5 +2,28 @@ function diceRandom() {
 
     var randomNumber1 = (Math.floor(Math.random() * 6)) + 1;
     var randomNumber2 = (Math.floor(Math.random() * 6)) + 1;
-  
+    return [randomNumber1,randomNumber2];
+}
+
+function diceImage () {
+    
+    var randomNumbers = diceRandom();
+    var randomNumber1 = randomNumbers[0];
+    var randomNumber2 = randomNumbers[1];
+    var image1 = document.querySelector(".img1");
+    
+    if (randomNumber1 == 1 ) {
+         var image1 = image1.setAttribute("src", "Assets/images/dice1.png")
+    } else if (randomNumber1 == 2) {
+         var image1 = image1.setAttribute("src", "Assets/images/dice2.png");
+    } else if (randomNumber1 == 3) {;
+         var image1 = image1.setAttribute("src", "Assets/images/dice3.png");         
+    } else if (randomNumber1 == 4) {
+         var image1 = image1.setAttribute("src", "Assets/images/dice4.png");
+    } else if (randomNumber1 == 5) {
+         var image1 = image1.setAttribute("src", "Assets/images/dice5.png");
+    } else {
+         var image1 = image1.setAttribute("src", "Assets/images/dice6.png");
+    }
+    
 }
